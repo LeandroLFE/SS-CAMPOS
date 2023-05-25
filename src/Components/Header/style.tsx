2e36a00004a89e37100d90ddd048ebc261d57068
Daiene <daiene.montuani@hotmail.com>
2023-05-24 22:21:33 -0300

    I fixed the header

--- src/Components/Header/style.tsx
+++ src/Components/Header/style.tsx
@@ -17,56 +17,44 @@ export const Container = styled.div`
         margin-left: 20px;
     }
     .logo {
-        width: 209px;
-        margin: 14px 38.7vw 14px 14px;
+        width: 30%;
+        max-width: 209px;
+        margin: 1%;
     }
 
-    .button {
+    button {
         text-transform: uppercase;
         border: none;
         background-color: rgba(0, 0, 0, 0);
         cursor: pointer;
-        font-size: 13.6px;
-        margin-top: 14px;
-        font-weight: 600;
+        font-size: 1vw; 
     }
 
-    .button:hover {
-        color: #FFCC29;
-    }
-
-    #button-1.active {
-        color: #FFCC29;
-      }
-      
-      #button-2.active {
-        color: #FFCC29;
-      }
-
-    #button-1 {  
-        margin-right: 10vw;
+    #button-1 {
+        color: #FFCC29;  
+        margin-left: 34%;
+        margin-right: 11%;
     }
 
     #button-2 {
-        margin-right: 8vw;
+        margin-right: 9.8%;
     }
-
-    .button-header.active {
-        color: #FFCC29;
-      }
 
     .icon {
-        margin-top: 6px;
-        margin-right: 20px;
-    }
-    
-    .icon:hover {
-        color: #FFCC29;
+        margin-top: 1%;
+        margin-right: 0.2%;
         cursor: pointer;
     }
 
-    a {
-        color: inherit;
-        text-decoration: none;
-      }
+    #wpp {
+        width: 1.4%;
+    }
+
+    #insta {
+        width: 1.6%;
+    }
+
+    #gmail {
+        width: 2%;
+    }
 `;
