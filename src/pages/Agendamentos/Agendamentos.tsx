f80576d546ec52d0d13a151c805a62cd548bb8bf
Daiene <daiene.montuani@hotmail.com>
2023-05-28 00:32:43 -0300

    Comments were placed on the scheduling page and the main title css and read more button were fixed

--- src/pages/Agendamentos/Agendamentos.tsx
+++ src/pages/Agendamentos/Agendamentos.tsx
@@ -1,33 +1,195 @@
-import Header from '../../Components/Header'
-import campos_schedules from '../../assets/campus_schedules.png';
-import { Container } from './styled';
+import { useRef } from 'react';
+import Header from "../../Components/Header";
+import campos_schedules from "../../assets/campus_schedules.png";
+import rafaelBarbosa from "../../assets/rafaelBarbosa.png";
+import erikaSeabra from "../../assets/erikaSeabra.png";
+import juniorFydelles from "../../assets/juniorFydelles.png";
+import mauricioFelix from "../../assets/mauricioFelix.png";
+import suzanneRodrigues from "../../assets/suzanneRodrigues.png";
+import { Container } from "./styled";
+import { FiChevronRight } from "react-icons/fi";
+import { ImQuotesLeft, ImQuotesRight } from "react-icons/im";
+import Footer from '../../Components/Footer';
 
-export default function Agendamentos () {
+export default function Agendamentos() {
+  const sectionButtonRef = useRef<HTMLHeadingElement>(null);
+
+  const handleButtonClick = () => {
+    sectionButtonRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };  
+
   return (
     <>
       <Header />
       <Container>
-      <div className="top-main">
-          <img src={campos_schedules} alt="Entrada de Campos do Jordão" className="campos_schedules"/>
-          <h1 className="principal-text">Pronto para viver o charme e o <br/>conforto de Campos do Jordão?</h1>
-          <button className="more-information" id='button-3'>Leia Mais &gt;</button>
-      </div>
-
-      <div className="description">
-          <h2 className="title-1">AGENDE JÁ SEU PASSEIO!</h2>
+        <img
+          src={campos_schedules}
+          alt="Entrada de Campos do Jordão"
+          className="campos_schedules"
+        />
+        <h1 className="principal-text">
+          Pronto para viver o charme e o <br />
+          conforto de Campos do Jordão?
+        </h1>
+         <button className="more-information" onClick={handleButtonClick}>
+          Leia Mais <FiChevronRight style={{ verticalAlign: "middle" }} />
+        </button>
+        {/* */}
+        <h2 className="title-1" ref={sectionButtonRef}>
+          AGENDE JÁ SEU PASSEIO!
+        </h2>
+        <br />
+        <div className="paragraph-description">
+          <p>
+            Quer viver uma experiência única em Campos do Jordão? Então, agende
+            já o seu passeio com a gente! É simples e rápido.
+          </p>
           <br />
-          <div className="paragraph-description">
-            <p>Quer viver uma experiência única em Campos do Jordão? Então, agende já o seu passeio com a gente! É simples e rápido.</p>
-            <br />
-            <p>Para marcar sua visita, basta clicar no botão de agendamento abaixo e você será direcionado para o nosso Whatsapp. Lá, nossa equipe estará pronta para lhe atender, esclarecer todas as suas dúvidas sobre nossos roteiros, nossas fotos e valores.</p>
-            <br />
-            <p>Oferecemos uma grande variedade de passeios turísticos em Campos do Jordão, desde as mais belas paisagens naturais até os pontos turísticos mais famosos da cidade. Confira nosso catálogo de fotos e escolha o roteiro ideal para você. Além disso, podemos personalizar o seu passeio de acordo com as suas preferências e necessidades. É só entrar em contato conosco pelo Whatsapp e contar o que você deseja. </p>
-            <br />
-            <p>
-Não perca a chance de conhecer a cidade mais charmosa da Serra da Mantiqueira! Agende agora mesmo e tenha uma experiência incrível em Campos do Jordão!</p>
-          </div>      
+          <p>
+            Para marcar sua visita, basta clicar no botão de agendamento abaixo
+            e você será direcionado para o nosso Whatsapp. Lá, nossa equipe
+            estará pronta para lhe atender, esclarecer todas as suas dúvidas
+            sobre nossos roteiros, nossas fotos e valores.
+          </p>
+          <br />
+          <p>
+            Oferecemos uma grande variedade de passeios turísticos em Campos do
+            Jordão, desde as mais belas paisagens naturais até os pontos
+            turísticos mais famosos da cidade. Confira nosso catálogo de fotos e
+            escolha o roteiro ideal para você. Além disso, podemos personalizar
+            o seu passeio de acordo com as suas preferências e necessidades. É
+            só entrar em contato conosco pelo Whatsapp e contar o que você
+            deseja.{" "}
+          </p>
+          <br />
+          <p>
+            {" "}
+            Não perca a chance de conhecer a cidade mais charmosa da Serra da
+            Mantiqueira! Agende agora mesmo e tenha uma experiência incrível em
+            Campos do Jordão!
+          </p>
+        </div>
+
+        <div className="button-schedule">
+          <button className="schedule">Agendar</button>
+        </div>
+
+        <h2 className="title-2">Depoimentos</h2>
+
+        <div className="container">
+          <div className="top-comments">
+            <div className="comment-box">
+              <div className="profile-info">
+                <img src={rafaelBarbosa} className="profile-picture" />
+                <p>
+                  {" "}
+                  Rafael Barbosa <br /> Rabelo Cruz{" "}
+                </p>
+                <br />
+              </div>
+              <ImQuotesLeft className="quote-icon-left" />
+              <p className="comment">
+                Valeu muito a pena contratar a empresa, atendimento excelente
+                que superou todas as expectativas! Pretendo visitar a cidade
+                novamente e irei contratar a empresa novamente. Guia Henrique
+                explica tudo direitinho e tira fotos excelentes! Parabéns pelo
+                trabalho.
+              </p>
+              <ImQuotesRight className="quote-icon-right" />
+            </div>
+
+            <div className="comment-box">
+              <div className="profile-info">
+                <img src={erikaSeabra} className="profile-picture" />
+                <p>
+                  {" "}
+                  Erika <br /> Seabra{" "}
+                </p>
+                <br />
+              </div>
+              <ImQuotesLeft className="quote-icon-left" />
+              <p className="comment">
+                Desde o primeiro contato com o motorista (Henrique), uma pessoa
+                maravilhosa, educado, simpático e atencioso fez o passeio ficar
+                ainda mais agradável. Teve toda a paciência em esperar até os
+                últimos minutos no passeio no santuário de aparecida.
+              </p>
+              <ImQuotesRight className="quote-icon-right" />
+            </div>
+
+            <div className="comment-box right-box" >
+              <div className="profile-info">
+                <img src={juniorFydelles} className="profile-picture" />
+                <p>
+                  {" "}
+                  Junior <br /> Fydelles{" "}
+                </p>
+                <br />
+              </div>
+              <ImQuotesLeft className="quote-icon-left" />
+              <p className="comment">
+                Sabe aquele passeio que se torna mais agradável ainda pelo fato
+                dos profissionais serem extremamente compromissados com o que
+                fazem? Só temos elogios! Desde o primeiro contato com o Sr
+                Henrique, até o nosso motorista e guia Álvaro. Que dia
+                maravilhoso! Que energia contagiante! Sem dúvidas, tornou nosso
+                momento ainda mais especial. Quero parabenizar e registrar o
+                diferencial da empresa em proporcionar a seus clientes não só um
+                passeio, mas uma experiência marcante. Gratidão! Já estou
+                programando a próxima!
+              </p>
+              <ImQuotesRight className="quote-icon-right" />
+            </div>
+          </div>
+
+          <div className="bottom-comments">
+            <div className="comment-box">
+              <div className="profile-info">
+                <img src={mauricioFelix} className="profile-picture" />
+                <p>
+                  {" "}
+                  Mauricio <br /> Felix{" "}
+                </p>
+                <br />
+              </div>
+              <ImQuotesLeft className="quote-icon-left" />
+              <p className="comment">
+                Compensou muito contratar o serviço do Henrique. Minha esposa e
+                eu não conhecíamos Campos do Jordão, por isso estávamos sem
+                saber pra onde ir. O nosso guia nos levou para os principais
+                pontos turísticos, tirou belas fotos, foi muito atencioso nas
+                explicações e conversou bastante com a gente. Tudo isso por um
+                preço justo.
+              </p>
+              <ImQuotesRight className="quote-icon-right" />
+            </div>
+
+            <div className="comment-box">
+              <div className="profile-info">
+                <img src={suzanneRodrigues} className="profile-picture" />
+                <p>
+                  {" "}
+                  Suzanne <br /> Rodrigues{" "}
+                </p>
+                <br />
+              </div>
+              <ImQuotesLeft className="quote-icon-left" />
+              <p className="comment">
+                Foi uma experiência incrível! O Eli foi super cordial e
+                atencioso desde o início ao final do passeio! fizemos o passeio
+                com ele no dia dos namorados, e apesar da cidade estar bastante
+                cheia, ele conseguiu tirar as melhores fotos, com ângulos
+                perfeitos! Recomendo demais
+              </p>
+              <ImQuotesRight className="quote-icon-right" />
+            </div>
+            
+            <div className="comment-box right-box" >
+            </div>
+          </div>
         </div>
       </Container>
+      <Footer />
     </>
   );
 }
--- src/pages/Agendamentos/styled.tsx
+++ src/pages/Agendamentos/styled.tsx
@@ -3,11 +3,12 @@ import styled from 'styled-components'
 export const Container = styled.div`
     width: 100vw; 
     background-color: black;
+    padding-bottom: 10rem;
 
     h2 {
         text-transform: uppercase;
         color: #FFCC29; 
-        font-size: 35px;
+        font-size: 2.43vw;
     }
 
     .campos_schedules {
@@ -17,29 +18,36 @@ export const Container = styled.div`
     }
 
     .principal-text {
-        display: absolute;
-        margin-top: -608px;
-        margin-left: 63px;
-        font-size: 50px;
+        position: absolute;
+        top: 50%;
+        left: 5%;
+        transform: translateY(-50%);
+        font-size: 3.5vw;
         font-weight: 550;
-        line-height: 60px;
+        line-height: 4vw;
         text-shadow: 1px 1px 12px #000000;
     }
 
     .more-information {
-        margin-top: 20px;
-        margin-left: 63px;
+        position: absolute;
+        top: 62%;
+        left: 5%;
         color: #000000;
-        background: #FFCC29; 
+        background: #FFCC29;
         text-transform: uppercase;
         font-weight: 550;
-        font-size: 0.97vw;
+        font-size: 1vw;
         border-radius: 50px;
         padding: 0.69vw 1.67vh;
+        cursor: pointer;
+    }
+
+    .more-information:hover {
+        color: #FFCC29;
+        background-color: rgba(255, 204, 41, 0.4);
     }
 
     .title-1 {
-        margin-top: 380px;
         text-align: center;
     }
 
@@ -48,4 +56,88 @@ export const Container = styled.div`
         margin-left: 19%;
         text-align: justify;
     }
+
+    .button-schedule {
+        display: flex;
+        justify-content: center;
+    }
+
+    .schedule {
+        font-size: 1vw;
+        text-transform: uppercase;
+        padding: 12px 30px;
+        background: #FFCC29;
+        color: black;
+        font-weight: bold;
+        border-radius: 50px;
+        margin-top: 3.4%;
+        cursor: pointer;
+    }
+
+    .schedule:hover {
+        color: #FFCC29;
+        background-color: rgba(255, 204, 41, 0.4);
+    }
+
+    .title-2 {
+        text-align: center;
+        margin-top: 9%;
+    }
+
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        height: 100vh;
+        margin-top: 25px;
+      }
+
+      .top-comments {
+          display: flex;
+      }
+
+      .bottom-comments {
+        display: flex;
+        margin-top: -120px;
+      }
+
+      .right-box {
+        margin-top: 100px
+      }
+      
+      .comment-box {
+        width: 300px;
+        height: 320px;
+        padding: 50px;
+      }
+
+      .profile-info {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+
+      .comment {
+        text-align: center;
+        font-style: italic;
+      }
+
+      .quote-icon-left {
+        color: #FFCC29;
+        font-size: 30px;
+        margin: 10px;
+        margin-left: -10px;
+        display: inline-block;
+      }
+
+      .quote-icon-right {
+        color: #FFCC29;
+        font-size: 30px;
+        margin: 10px;
+        margin-left: 270px;
+      }
+
+      .profile-picture {
+        margin-right: 20px
+      }
 `;
