a1d3581a81662f9904d1ab4bca804a395a3859ca
Daiene <daiene.montuani@hotmail.com>
2023-05-30 09:48:09 -0300

    Link of buttons in footer fixed

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -2,6 +2,41 @@
 import logo from '../../assets/logo_horizontal.png';
 import { FaWhatsapp, FaInstagram } from "react-icons/fa";
 import { Container } from './style';
+import { Link, useLocation } from "react-router-dom";
+import styled from 'styled-components';
+
+interface LinkProps {
+  isActive: boolean;
+}
+
+const CustomLink = styled(Link)<LinkProps>`
+  color: ${(props) => props.isActive ? '#FFCC29' : 'white'};
+  &:hover {
+    color: #FFCC29;
+  }
+`;
+
+interface ButtonProps {
+  to: string;
+  className?: string;
+  id?: string;
+  children?: React.ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = ({ to, className, id, children }) => {
+  const active = useLocation().pathname === to;
+
+  return (
+    <CustomLink
+      to={to}
+      className={className}
+      id={id}
+      isActive={active}
+    >
+      {children}
+    </CustomLink>
+  );
+};
 
 const Footer = () => {
   return (
@@ -11,22 +46,20 @@ const Footer = () => {
             <img src={logo} alt="logo SS PASSEIOS EM CAMPOS DO JORDÃO" className="logo" />
             <div className="paginas">
                 <p className="title-paginas">PÁGINAS</p>
-                <button
-                    className={`button ${
-                        location.pathname === "/inicio" ? "active" : ""
-                    }`}
-                    id="button-1"
-                    onClick={() => (window.location.href = "./inicio")}>
-                    Início
-                </button>
-                <button
-                    className={`button ${
-                        location.pathname === "/agendamentos" ? "active" : ""
-                    }`}
-                    id="button-2"
-                    onClick={() => (window.location.href = "./agendamentos")}>
-                    Agendamentos
-                </button>
+                <Button
+                to="/inicio"
+                className={`button ${location.pathname === "/inicio" ? "active" : ""}`}
+                id="button-1"
+                >
+                Início
+                </Button>
+                <Button
+                to="/agendamentos"
+                className={`button ${location.pathname === "/agendamentos" ? "active" : ""}`}
+                id="button-2"
+                >
+                Agendamentos
+                </Button>
                 <p className="devs">&copy; Desenvolvido por Daiene Batagioti e Sarah Batagioti</p>
             </div>
             <div className="contact">
